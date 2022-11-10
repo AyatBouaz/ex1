48c8e040dc3663122350f332b95d7cea56ee4b33
dell <dell@DESKTOP-OFLO969>
2022-11-10 22:33:19 +0100

    Ajoutez des fichiers projet.

---
+++ ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+    internal class Exam
+    {
+        private int nb_question;
+        private String nom;
+        private int durée_dexam;
+
+        public Exam() { }
+
+
+    }
+}
