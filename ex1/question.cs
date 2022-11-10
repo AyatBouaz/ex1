c0ea22fb5c9b0a5a4e68d7a89e0113963df9c3d5
AyatBouaz <116873488+AyatBouaz@users.noreply.github.com>
2022-11-10 22:57:56 +0100

    Create question.cs

---
+++ ex1/question.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAM
+{
+       public class Question
+    {
+        private string typeqst;
+        private int numques;
+        private int point;
+         
+
+    }
+    public class Vraifaux : Question  {
+
+    }
+    public class Qcm : Question
+    {
+
+    }
+    
+
+   }
+}
